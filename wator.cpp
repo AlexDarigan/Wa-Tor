526da33bfaee1dde934178f766d7857d9b4ac190
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2023-11-10 01:02:24 +0000

    Enable single-fish control test
    Problems we had (now fixed)
    - We were using value types so the change wasn't being picked up
      properly (I think), instead we modify them in place in the array
    
    - When a fish wasn't moving, we forgot to record that in our future
    rec2, alsowhen a fish wasn't moving because it was blocked, we overwrote
    the blocking fish with ocean instead of keeping it around.

--- wator.cpp
+++ wator.cpp
@@ -29,6 +29,8 @@
 // Code:
 
 #include <SFML/Graphics.hpp>
+#include <cstdlib> 
+
 
 sf::Color Ocean = sf::Color::Blue;
 sf::Color Fish = sf::Color::Green;
@@ -41,8 +43,10 @@ const int ydim= 100;
 sf::RectangleShape recArray[xdim][ydim];
 sf::RectangleShape recArray2[xdim][ydim];
 
+
 int main()
 {
+  srand(0);
   int WindowXSize=800;
   int WindowYSize=600;
   int cellXSize=WindowXSize/xdim;
@@ -55,46 +59,55 @@ int main()
   for(int i=0;i<xdim;++i){
     recArray[xdim][ydim].setFillColor(Ocean);
     for(int k=0;k<ydim;++k){//give each one a size, position and color
+      recArray2[i][k].setFillColor(Ocean);
       recArray[i][k].setSize(sf::Vector2f(80.f,80.f));
       recArray[i][k].setPosition(i*cellXSize,k*cellYSize);//position is top left corner!
-      int id=i*1-+k;
-      if (id%2==0) recArray[i][k].setFillColor(Fish);
+      //int id=i*1-+k;
+      //if (id%2==0) recArray[i][k].setFillColor(Fish);
+      if(i == 0 && k == 0) { recArray[0][0].setFillColor(Fish); }
       else recArray[i][k].setFillColor(Ocean);
     }
   }
     sf::RenderWindow window(sf::VideoMode(WindowXSize,WindowYSize), "SFML Wa-Tor world");
    
 
-
     while (window.isOpen())
     {
+ 
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-	
-      // current = future
-      // future = new Array()
 
       // MoveFishLeft + New Fish
       for(int y = 0; y < ydim; ++y) {
         for(int x = 0; x < xdim; ++x) {
-          //printf("%d,%d", x, y);
           int destination = (x + 1) % xdim;
-          sf::RectangleShape prevCell = recArray[x][y];
-          sf::RectangleShape nextCell = recArray[(x + 1) % xdim][y];
-
-          bool isFish = prevCell.getFillColor() == Fish;
-          bool isBlocked = nextCell.getFillColor() == Fish;
-          if(isFish && !isBlocked) {
+          bool isFish = recArray[x][y].getFillColor() == Fish;
+          bool isNotBlocked = recArray[destination][y].getFillColor() == Ocean;
+          bool willMoveNow = (rand() % 2) == 1; // 1 - Yes / 0 - No
+          if(isFish && willMoveNow && isNotBlocked) {
             recArray2[destination][y].setFillColor(Fish);
+            recArray2[x][y].setFillColor(Ocean);
           } else {
-            printf("%s", "No Move Fish\n");
+            // If we're not moving, we still need to state that in rect2
+            if(isFish) {
+              recArray2[x][y].setFillColor(Fish); 
+            }
+            else {
+              recArray[x][y].setFillColor(Ocean);
+            }
+            if(isNotBlocked) {
+              // Turn water into wine but not fish into the Ocean, that's for the sharks
+              recArray2[destination][y].setFillColor(Ocean);
+            }
+            printf("IsFish: %d, IsNotBlocked: %d, willMoveNow: %d\n", isFish, isNotBlocked, willMoveNow);
           }
         }
       }
+
       // MoveFishRight + New Fish
       // MoveFishUp + New Fish
       // MoveFishDown + New Fish
@@ -107,13 +120,18 @@ int main()
     for(int y = 0; y < ydim; ++y) {
       for(int x = 0; x < xdim; ++x) {
         recArray[x][y].setFillColor(recArray2[x][y].getFillColor());
-        recArray2[x][y].setFillColor(Ocean);
+        //recArray2[x][y].setFillColor(Ocean);
       }
     }
+
+    
+
+    
+
 	
 	
 	//loop these three lines to draw frames
-        window.clear(sf::Color::Black);
+  window.clear(sf::Color::Black);
 	for(int i=0;i<xdim;++i){
 	  for(int k=0;k<ydim;++k){
 	    window.draw(recArray[i][k]);
@@ -121,6 +139,9 @@ int main()
 	}
         window.display();
     }
+
+    
+
 
     return 0;
 }
